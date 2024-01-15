fe333ad06c4ce8637e38216af91dba6b2ee05573
metacardBuilder <colladomaykelin@gmail.com>
2024-01-15 23:38:45 +0530

    Create 2225. Find Players With Zero or One Losses.py

---
+++ 2225. Find Players With Zero or One Losses.py
@@ -0,0 +1,76 @@
+'''
+You are given an integer array matches where matches[i] = [winneri, loseri] indicates that the player winneri defeated player loseri in a match.
+
+Return a list answer of size 2 where:
+
+answer[0] is a list of all players that have not lost any matches.
+answer[1] is a list of all players that have lost exactly one match.
+The values in the two lists should be returned in increasing order.
+
+Note:
+
+You should only consider the players that have played at least one match.
+The testcases will be generated such that no two matches will have the same outcome.
+ 
+
+Example 1:
+
+Input: matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
+Output: [[1,2,10],[4,5,7,8]]
+Explanation:
+Players 1, 2, and 10 have not lost any matches.
+Players 4, 5, 7, and 8 each have lost one match.
+Players 3, 6, and 9 each have lost two matches.
+Thus, answer[0] = [1,2,10] and answer[1] = [4,5,7,8].
+Example 2:
+
+Input: matches = [[2,3],[1,3],[5,4],[6,4]]
+Output: [[1,2,5,6],[]]
+Explanation:
+Players 1, 2, 5, and 6 have not lost any matches.
+Players 3 and 4 each have lost two matches.
+Thus, answer[0] = [1,2,5,6] and answer[1] = [].
+ 
+
+Constraints:
+
+1 <= matches.length <= 105
+matches[i].length == 2
+1 <= winneri, loseri <= 105
+winneri != loseri
+All matches[i] are unique.
+'''
+
+'''
+Intuition
+The problem involves tracking the outcomes of matches and categorizing players based on their losses. The task is to identify players who have not lost any matches and those who have lost exactly one match.
+
+Approach
+Utilize an array losses to keep track of the number of losses for each player.
+Iterate through the matches array and update the losses array accordingly.
+Create two lists, zeroLoss and oneLoss, to store players with zero and one loss, respectively.
+Populate these lists based on the information in the losses array.
+Return a list containing zeroLoss and oneLoss.
+Complexity
+Time complexity: The time complexity is O(N), where N is the length of the matches array, as we iterate through the matches once.
+Space complexity: The space complexity is O(M), where M is the number of players (limited to 100,001), for storing the losses array and result lists.
+'''
+
+
+class Solution:
+    def findWinners(self, matches):
+        losses = [0] * 100001
+
+        for winner, loser in matches:
+            if losses[winner] == 0:
+                losses[winner] = -1
+
+            if losses[loser] == -1:
+                losses[loser] = 1
+            else:
+                losses[loser] += 1
+
+        zero_loss = [i for i in range(1, 100001) if losses[i] == -1]
+        one_loss = [i for i in range(1, 100001) if losses[i] == 1]
+
+        return [zero_loss, one_loss]
